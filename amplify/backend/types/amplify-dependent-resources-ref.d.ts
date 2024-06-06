ed73814f58ecc42f17126c85bfeef4e2e4dacfc8
Dr. Michael Kroll <dr.michael.kroll@gmail.com>
2024-06-06 09:45:48 +0200

    Amplify backend added.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "enchantedrealms": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "enchantedrealmse27d3bf7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "enchantedrealms": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
