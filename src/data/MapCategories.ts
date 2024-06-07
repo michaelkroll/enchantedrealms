4fae39bbd5a276dee44803c7a5b2843d109cb29f
Dr. Michael Kroll <dr.michael.kroll@gmail.com>
2024-06-07 16:39:03 +0200

    Subtypes removed.

--- src/data/MapCategories.ts
+++ src/data/MapCategories.ts
@@ -2,13 +2,14 @@ import Category from "./Category";
 
 const mapCategories:Category[] =[
   {value: "city", label: "City"},
+  {value: "coast", label: "Coast"},
   {value: "dungeon", label: "Dungeon"},
   {value: "forrest", label: "Forrest"},
-  {value: "forrest_day", label: "Forrest - Day"},
-  {value: "forrest_night", label: "Forrest - Night"},
+  {value: "lake", label: "Lake"},
   {value: "mansion", label: "Mansion"},
   {value: "mountains", label: "Mountain"},
   {value: "misc", label: "Miscellaneous"},
+  {value: "river", label: "River"},
   {value: "ship", label: "Ship"},
   {value: "tomb", label: "Tomb"},
   {value: "uncategorized", label: "Uncategorized"},
