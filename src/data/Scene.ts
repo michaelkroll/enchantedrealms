bd42b25eefae9d76141c1122d851819b79ec206c
Dr. Michael Kroll <dr.michael.kroll@gmail.com>
2024-06-09 11:32:46 +0200

    Removed unused attributes.

--- src/data/Scene.ts
+++ src/data/Scene.ts
@@ -4,13 +4,6 @@ interface Scene {
   creatorEmail: string;
   creatorId: string;
   description?: string | null | undefined;
-  adventureId: string;
-  adventureName: string;
-  mapId: string;
-  mapName: string;
-  mapPicPath?: string | null | undefined;
-  mapPicS3Url?: string | null | undefined;
-  entityIds?: (string | null)[] | null | undefined;
 }
 
 export default Scene;
