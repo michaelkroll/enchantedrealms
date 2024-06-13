9e35a8760005acaf78af39cf6cb8b4a44937d734
Dr. Michael Kroll <dr.michael.kroll@gmail.com>
2024-06-13 09:39:08 +0200

    Detail view for entities added.

--- src/components/entities/EntityDisplayModal.tsx
+++ src/components/entities/EntityDisplayModal.tsx
@@ -1,5 +1,8 @@
 import {
+  Box,
   Button,
+  Grid,
+  GridItem,
   Image,
   Modal,
   ModalBody,
@@ -21,14 +24,20 @@ interface Props {
 
 const EntityDisplayModal = ({ isOpen, onClose, entity }: Props) => {
   return (
-    <Modal onClose={onClose} isOpen={isOpen} isCentered>
+    <Modal size="2xl" onClose={onClose} isOpen={isOpen} isCentered>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>{entity.name}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Image src={entity.tokenPicS3Url!} />
-          <Text>{entity.description}</Text>
+          <Grid templateRows="2" templateColumns="3" gap={2}>
+            <GridItem rowSpan={2} colSpan={1}>
+              <Image src={entity.tokenPicS3Url!} />
+            </GridItem>
+            <GridItem colSpan={3}>
+              <Text>{entity.description}</Text>
+            </GridItem>
+          </Grid>
         </ModalBody>
         <ModalFooter>
           <Button onClick={onClose}>Close</Button>
