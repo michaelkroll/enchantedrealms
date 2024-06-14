f8b39638789f03ee753e394c5aac1954d239a914
Dr. Michael Kroll <dr.michael.kroll@gmail.com>
2024-06-14 22:29:30 +0200

    removed a lot of styling

--- src/components/room/Room.tsx
+++ src/components/room/Room.tsx
@@ -1,12 +1,4 @@
-import {
-  Box,
-  Button,
-  Container,
-  Divider,
-  Heading,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Button, Container, Divider, Stack, Text } from "@chakra-ui/react";
 import { useNavigate, useParams } from "react-router-dom";
 
 interface Props {
@@ -23,38 +15,21 @@ const Room = ({ email, sub }: Props) => {
   console.log("Sub: ", sub);
 
   return (
-    <Box as="section">
-      <Container py={{ base: "16", md: "24" }}>
-        <Stack spacing={{ base: "8", md: "10" }}>
-          <Stack spacing={{ base: "4", md: "5" }} align="center">
+    <Box>
+      <Container>
+        <Stack>
+          <Stack mt={2} align="center">
             <Divider />
-            <Heading size={{ base: "sm", md: "md" }}>
-              This is the Room for Adventure
-            </Heading>
-            <Text maxW="2xl" textAlign="center" fontSize="xl">
-              "{params.adventureId}"
-            </Text>
+            <Text as="b">This is the Room for Adventure</Text>
+            <Text>"{params.adventureId}"</Text>
             <Divider />
-            <Heading size={{ base: "sm", md: "md" }}>
-              You have joined as user
-            </Heading>
-            <Text maxW="2xl" textAlign="center" fontSize="xl">
-              "{email}"
-            </Text>
-            <Divider />
-            <Text maxW="2xl" textAlign="center" fontSize="xl">
-              Here the scenes will be placed by the game master and entities can
-              be moved by the GM and the players
-            </Text>
+            <Text as="b">You have joined as user</Text>
+            <Text>"{email}"</Text>
             <Divider />
           </Stack>
-          <Stack
-            spacing="3"
-            direction={{ base: "column", sm: "row" }}
-            justify="center"
-          >
+          <Stack direction={{ base: "column", sm: "row" }} justify="center">
             <Button
-            colorScheme='blue'
+              colorScheme="blue"
               onClick={() => {
                 navigate("/adventures");
               }}
